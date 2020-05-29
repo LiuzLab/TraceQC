671970b2198e67cf67084e4195ccfcf59b124347
Jingyuan Hu <Jingyuan.Hu@bcm.edu>
2020-05-29 18:44:16 -0500

    update alignment method

--- inst/py/alignment.py
+++ inst/py/alignment.py
@@ -5,6 +5,16 @@ import pandas as pd
 from Bio import pairwise2, SeqIO
 from multiprocessing import Process, Pool
 
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--input","-f",help="input sequencing file")
+# parser.add_argument("--reference","-r",help="reference sequence file")
+# parser.add_argument("--match","-a",help="match score for alignment",default=2)
+# parser.add_argument("--mismatch","-i",help="mismatch score for alignment",default=-2)
+# parser.add_argument("--gapopen","-g",help="gap opening score for alignment",default=-6)
+# parser.add_argument("--gapextension","-e",help="gap extension score for alignment",default=-0.1)
+# parser.add_argument("--output","-o",help="output file",default="./output/aligned.txt")
+# args = parser.parse_args()
+
 def alignment(args):
     _,args["format"] = os.path.splitext(args["input"])
     regions = {}
@@ -31,13 +41,8 @@ def alignment(args):
             seq = s.seq._data
             data["name"].append(s.name)
 
-        align = pairwise2.align.globalms(ref,
-                                    seq,
-                                    args["match"],
-                                    args["mismatch"],
-                                    args["gapopen"],
-                                    args["gapextension"],
-                                    one_alignment_only=True)[0]
+        align = pairwise2.align.globalms(ref,seq,args["match"],args["mismatch"],\
+                                     args["gapopen"],args["gapextension"],one_alignment_only=True)[0]
         data["seq"].append(align[1])
         data["ref"].append(align[0])
         data["score"].append(align[2])
@@ -57,3 +62,4 @@ def alignment(args):
     result = pd.DataFrame(data=data)
     result.to_csv(args["output"],sep="\t",index=False)
 
+# alignment(args)
