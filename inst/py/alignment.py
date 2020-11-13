6d81f95f3c242f6642082c3494966951e761430c
JasperYH <jingyuanhu0626@gmail.com>
2020-11-13 15:07:34 -0600

    simplify alignment

--- inst/py/alignment.py
+++ inst/py/alignment.py
@@ -8,8 +8,8 @@ from tqdm import tqdm
 import gzip
 
 
-def get_score(param):
-    s, ref, regions, args = param
+def get_alignment_result(param):
+    s, ref, target_start, target_end, args = param
     seq = s.seq._data
     align = pairwise2.align.globalms(ref,
                                 seq,
@@ -20,33 +20,25 @@ def get_score(param):
                                 one_alignment_only=True)[0]
 
     cs = np.cumsum([i!="-" for i in align[0]])
-    
+
     data = {}
     data["name"] = s.name
     data["seq"] = align[1]
     data["ref"] = align[0]
     data["score"] = align[2]
-    
-    for region in regions:
-        start = min(np.argwhere(cs==regions[region][0])[0])
-        end = min(np.argwhere(cs==regions[region][1])[0])
-        data["%s_ref"%region] = align[0][start:end]
-        data["%s_seq"%region] = align[1][start:end]
-        
+    start = min(np.argwhere(cs==target_start)[0])
+    end = min(np.argwhere(cs==target_end)[0])
+    data["target_ref"] = align[0][target_start:target_end]
+    data["target_seq"] = align[1][target_start:target_end]
     return data
 
 def alignment(args):
-    regions = {}
     with open(args["reference"],"r") as f:
         ref = f.readline().strip()
         for line in f:
             region,start,end = line.strip().split(" ")
-            regions[region] = (int(start),int(end)+1)
-
-    data = {"name":[],"seq":[],"ref":[],"score":[]}
-    for region in regions:
-        data["%s_seq"%region] = []
-        data["%s_ref"%region] = []
+            if region == "target":
+                target_start,target_end = int(start),int(end)
 
     inp_handle = None
     if args["input"].lower().endswith("gz"):
@@ -55,18 +47,18 @@ def alignment(args):
         inp_handle = open(args["input"], "r")
 
     nreads = 0
-    
+
     if int(args["ncores"]) != 1:
         with Pool(processes=int(args["ncores"])) as pool:
             data = [ret for ret in \
-                tqdm(pool.imap_unordered(get_score,((s, ref, regions, args) \
+                tqdm(pool.imap_unordered(get_alignment_result,((s, ref, target_start, target_end, args) \
                 for s in SeqIO.parse(inp_handle, "fastq")))) ]
-    else: 
+    else:
         data = [ret for ret in \
-            tqdm(get_score((s, ref, regions, args)) \
+            tqdm(get_alignment_result((s, ref, target_start, target_end, args)) \
             for s in SeqIO.parse(inp_handle, "fastq")) ]
-    
-    
+
+
     if inp_handle != None:
         inp_handle.close()
 
