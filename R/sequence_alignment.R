ec2c7f57c734aa90556f4fd29b721f478a921d54
JasperYH <jingyuanhu0626@gmail.com>
2021-01-27 16:36:54 -0600

    sequence alignment for 10x

--- R/sequence_alignment.R
+++ R/sequence_alignment.R
@@ -68,6 +68,64 @@ sequence_alignment <- function(input_file,
   }
 }
 
+#' Function for a sequence alignment between the reference file and sample for 10x data.
+#'
+#' The function is an wrapper of a python function which performs
+#' a global pairwise sequence alignment by biopython package.
+#'
+#' @param input_file  A file path of possorted_genome_bam.bam out put by cellranger
+#' @param ref_file A path of a reference sequence file.
+#' @param output_file The output path. An output of the alignment will be
+#' stored at the path.
+#' @param match The score for a correct basepair matching.
+#' @param mismatch The penalty score for a basepair mismatching.
+#' @param gapopen The gap opening score for the alignment.
+#' @param gapextension The gap extension score for the alignment.
+#' @param return_df A logical argument to report what type of output will
+#' be created from the function.
+#' @param ncores The number of cores for the parallel processing
+#'
+#' @importFrom reticulate source_python
+#' @importFrom readr read_tsv
+#' @importFrom tictoc tic toc
+#' @return It returns a data frame of the alignment result
+#' if `return_df' is `T' and `NULL' otherwise.
+#' @export
+#'
+#' @examples
+sequence_alignment_for_10x <- function(input_file,
+                               ref_file,
+                               output_file="aligned_reads.txt",
+                               match=2,
+                               mismatch=-2,
+                               gapopen=-6,
+                               gapextension=-0.1,
+                               ncores = 4,
+                               return_df = FALSE) {
+  args <- list("input"=get_abspath(input_file),
+               "reference"=get_abspath(ref_file),
+               "output"=get_abspath(output_file),
+               "match"=match,"mismatch"=mismatch,"gapopen"=gapopen,
+               "gapextension"=gapextension,
+               "ncores"=ncores)
+  source_python(system.file("py", "alignment_for_10x.py", package="TraceQC"))
+  print(ls(parent.frame()))
+  module <- reticulate::import_from_path("alignment_for_10x", system.file("py", package = "TraceQC"))
+
+
+  tic("Alignment")
+  message(paste0("Running an alignment between ", ref_file,
+                 " and ", input_file, "."))
+  module$alignment(args)
+  toc()
+
+  if(return_df) {
+    read_tsv(output_file)
+  } else {
+    return(NULL)
+  }
+}
+
 #' Function for finding threshold of sequence alignment. The function randomly
 #' permutate certain percentage reference sequence and perform global alignment
 #' with the original reference sequence. By use the permutated sequence alignment
---
+++ inst/py/alignment_for_10x.py
@@ -0,0 +1,73 @@
+import os
+import argparse
+import numpy as np
+import pandas as pd
+from Bio import pairwise2, SeqIO
+from multiprocessing import Process, Pool
+from tqdm import tqdm
+import gzip
+import pysam
+
+def get_alignment_result(param):
+    s, ref, target_start, target_end, args = param
+    align = pairwise2.align.globalms(ref,
+                                s["query_sequence"],
+                                args["match"],
+                                args["mismatch"],
+                                args["gapopen"],
+                                args["gapextension"],
+                                one_alignment_only=True)[0]
+
+    cs = np.cumsum([i!="-" for i in align[0]])
+
+    data = {}
+    data["name"] = s["qname"]
+    data["seq"] = align[1]
+    data["ref"] = align[0]
+    data["score"] = align[2]
+    target_start = min(np.argwhere(cs==target_start)[0])
+    target_end = min(np.argwhere(cs==target_end)[0])
+    data["target_ref"] = align[0][target_start:target_end]
+    data["target_seq"] = align[1][target_start:target_end]
+    data["CB"] = s["CB"]
+    data["UB"] = s["UB"]
+    return data
+
+def parse_10x_data(args):
+    align_file = pysam.AlignmentFile(args["input"], "rb")
+    for seq in align_file:
+        if seq.is_unmapped: 
+            s = {}
+            s["query_sequence"] = seq.query_sequence
+            s["qname"] = seq.qname
+            try:
+                s["CB"] = seq.get_tag("CB")
+            except:
+                s["CB"] = "/"
+            try:
+                s["UB"] = seq.get_tag("UB")
+            except:
+                s["UB"] = "/"
+            yield s
+    
+def alignment(args):
+    with open(args["reference"],"r") as f:
+        ref = f.readline().strip()
+        for line in f:
+            region,start,end = line.strip().split(" ")
+            if region == "target":
+                target_start,target_end = int(start),int(end)
+
+    seqs = parse_10x_data(args)
+    if int(args["ncores"]) != 1:
+        with Pool(processes=int(args["ncores"])) as pool:
+            data = [ret for ret in \
+                tqdm(pool.imap_unordered(get_alignment_result,[(s, ref, target_start, target_end, args) \
+                for s in seqs]))]
+    else:
+        data = [ret for ret in \
+            tqdm(get_alignment_result((s, ref, target_start, target_end, args)) \
+            for s in seqs) ]
+
+    result = pd.DataFrame(data=data)
+    result.to_csv(args["output"],sep="\t",index=False)
