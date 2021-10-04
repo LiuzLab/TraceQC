1214a63844d32210c626450aa73b3ec47c57d9e7
jasper <jingyuanhu@Jaspers-Macbook.local>
2021-10-03 19:51:08 -0500

    tutorials

--- inst/py/alignment_for_10x.py
+++ inst/py/alignment_for_10x.py
@@ -12,11 +12,11 @@ def get_alignment_result(param):
     s, ref, target_start, target_end, args = param
     align = pairwise2.align.globalms(ref,
                                 s["query_sequence"],
-                                args["match"],
-                                args["mismatch"],
-                                args["gapopen"],
-                                args["gapextension"],
-                                penalize_end_gaps=int(args["penalize_end_gaps"]),
+                                args.match,
+                                args.mismatch,
+                                args.gapopen,
+                                args.gapextension,
+                                penalize_end_gaps=args.penalize_end_gaps,
                                 one_alignment_only=True)
 
     if align:
@@ -38,8 +38,8 @@ def get_alignment_result(param):
     return
 
 def parse_10x_data(args):
-    align_file = pysam.AlignmentFile(args["input"], "rb")
-    for seq in align_file:
+    align_file = pysam.AlignmentFile(args.input, "rb")
+    for seq in align_file.fetch(until_eof=True):
         if seq.is_unmapped: 
             s = {}
             s["query_sequence"] = seq.query_sequence
@@ -55,7 +55,7 @@ def parse_10x_data(args):
             yield s
     
 def alignment(args):
-    with open(args["reference"],"r") as f:
+    with open(args.ref,"r") as f:
         ref = f.readline().strip()
         for line in f:
             region,start,end = line.strip().split(" ")
@@ -63,8 +63,8 @@ def alignment(args):
                 target_start,target_end = int(start),int(end)
 
     seqs = parse_10x_data(args)
-    if int(args["ncores"]) != 1:
-        with Pool(processes=int(args["ncores"])) as pool:
+    if int(args.ncores) != 1:
+        with Pool(processes=int(args.ncores)) as pool:
             data = [ret for ret in \
                 tqdm(pool.imap_unordered(get_alignment_result,[(s, ref, target_start, target_end, args) \
                 for s in seqs]))]
@@ -74,4 +74,18 @@ def alignment(args):
             for s in seqs)]
     data = [d for d in data if d]
     result = pd.DataFrame(data=data)
-    result.to_csv(args["output"],sep="\t",index=False)
+    result.to_csv(args.output,sep="\t",index=False)
+    
+if __name__ == "__main__"   :
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ref', help='the path of reference file')
+    parser.add_argument('--input', help='the path of bam file')
+    parser.add_argument('--ncores', help='the number of cores to use',default=1)
+    parser.add_argument('--output', help='the path of output file')
+    parser.add_argument('--match', help='the penalty score for matched character', type=int, default=2)
+    parser.add_argument('--mismatch', help='the penalty score for mismatched character', type=int, default=-2)
+    parser.add_argument('--gapopen', help='the penalty score for gap opening', type=int, default=-6)
+    parser.add_argument('--gapextension', help='the penalty score for gap extension', type=float, default=-0.1)
+    parser.add_argument('--penalize_end_gaps', help='if penalize the end gaps: 0 or 1', type=int, default=1)
+    args = parser.parse_args()
+    alignment(args)
